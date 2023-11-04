a7f3105890c6e1dde920700d3e6974e0d2c1572a
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2023-11-04 16:56:35 -0700

    add next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
-import '../styles/global.css';
+import '../styles/global.css'
+import { SessionProvider } from 'next-auth/react'
+import { Session } from 'next-auth'
 
-import type { AppProps } from 'next/app';
+import type { AppProps } from 'next/app'
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({
+    Component,
+    pageProps: { session, ...pageProps },
+}: AppProps & { session: Session }) => (
+    <SessionProvider session={session}>
+        <Component {...pageProps} />
+    </SessionProvider>
+)
 
-export default MyApp;
+export default MyApp
