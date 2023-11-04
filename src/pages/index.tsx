d4ea630e17f171bc027daef0be9446e78aa512cc
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2023-11-04 15:41:14 -0700

    add non-functional login button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import { Meta } from '@/layouts/Meta';
+import { Login } from '@/components/Login'
+import { Meta } from '@/layouts/Meta'
 
 const Index = () => {
-  return (
-    <div className="flex min-h-screen min-w-full flex-col items-center border border-black">
-      <Meta
-        title="Next.js Boilerplate Presentation"
-        description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
-      />
-      <h1 className="text-2xl font-bold">Next.js Boilerplate</h1>
-    </div>
-  );
-};
+    return (
+        <div className='flex min-h-screen min-w-full flex-col items-center'>
+            <Meta title='Dev Estimator' description='' />
+            <div className='mt-16'>
+                <Login />
+            </div>
+        </div>
+    )
+}
 
-export default Index;
+export default Index
