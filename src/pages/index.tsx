8f5c15e1c5ef5955cde206ecfe1b6d6aa21a753e
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2024-02-25 22:26:24 -0800

    remove home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,26 @@
-import { BasicButton } from '@/components/BasicButton'
 import { Meta } from '@/layouts/Meta'
-import { useRouter } from 'next/router'
-import App from './app'
-const Index = () => {
-    const router = useRouter()
+import { OctokitContextProvider } from '@/contexts/OctokitContextProvider'
+import { AllRepoMeta } from '@/components/AllRepoMeta'
+import { BasicButton } from '@/components/BasicButton'
+import { Toaster } from 'react-hot-toast'
+import PageWrapper from '@/layouts/PageWrapper'
 
+const Index = () => {
     return (
-        <div className='flex min-h-screen min-w-full flex-col items-center bg-stone-800 text-white px-8'>
-            <Meta
-                title='Welcome to Dev Estimator'
-                description='Explore the app to estimate your development tasks.'
-            />
-            <h1>
-                Welcome to AutoDev.AI
-            </h1>
-            <BasicButton text='Enter App' onClick={() => {
-                router.push('/app')
-            }}/>
-        </div>
+        <PageWrapper>
+            <Meta title='Chat With Your Code' description='' />
+            <Toaster />
+            {/* <BasicButton
+                text='Go Back'
+                onClick={() => {
+                    window.history.back()
+                }}
+            /> */}
+            <h1 className='text-3xl font-bold mt-8'>Chat With Your Code</h1>
+            <OctokitContextProvider>
+                <AllRepoMeta />
+            </OctokitContextProvider>
+        </PageWrapper>
     )
 }
 
